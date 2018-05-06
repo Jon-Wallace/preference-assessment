f5ca415a91e9d261d5f3274fd6d813e89d0bfa88
Jon Wallace <jon@jons-mbp.lan>
2018-05-06 14:05:06 -0400

    Randomize selections

--- scripts.js
+++ scripts.js
@@ -1,51 +1,6 @@
 var combinations = [];
-var current = 0;
-var dictionary = {};
-
-function setImages(index) {
-    var combination = combinations[index];
-    $('#leftHidden').attr('value', combination.first);
-    $('#leftSelector').attr('src', 'images/' + 
-        combination.first + '.png');
-
-    $('#rightHidden').attr('value', combination.second);
-    $('#rightSelector').attr('src', 'images/' + 
-        combination.second + '.png');
-
-    $(".progressContainer").text(
-        (index + 1) + " of " + combinations.length);
-}
-
-function advanceImages(selection) {
-    if(dictionary.hasOwnProperty(selection)) {
-        dictionary[selection] = dictionary[selection] + 1;
-    }
-    else {
-        dictionary[selection] = 1;
-    }
-
-    current = current + 1;
-
-    if(current < combinations.length) {
-        setImages(current);
-    }
-    else {
-        displaySummary();
-    }
-}
-
-function displaySummary() {
-    $("#heading").text("Results");
-    $(".selectorContainer").hide();
-    $(".progressContainer").hide();
-    $(".resultsContainer").show();
-
-    for(var key in dictionary){
-      var value = dictionary[key];
-      console.log(key + ' = ' + value);
-      $("#resultsText").append("<p>" + key + " : " + value + "</p>");
-    }
-}
+var currentCombination = 0;
+var selectionsMap = {};
 
 $(function(){
     
@@ -75,10 +30,15 @@ $(function(){
         'skittles'
         ];
 
+    // add keys to dictionary
     for (var i = 0; i <= list.length - 1; i++) {
-        dictionary[list[i]] = 0;
+        selectionsMap[list[i]] = 0;
     }
 
+    // shuffle list
+    shuffle(list);
+
+    // compute combinations
     for (var i = 0; i < list.length - 1; i++) {
         for (var j = i; j < list.length - 1; j++) {
             combinations.push(
@@ -89,7 +49,10 @@ $(function(){
         }
     };
 
-    console.log(combinations.length);
+    // shuffle combinations
+    shuffle(combinations);
+
+    console.log("There are " + combinations.length + " combinations");
 
     for (var i = 0; i < combinations.length - 1; i++) {
         console.log(combinations[i].first + 
@@ -97,6 +60,70 @@ $(function(){
             combinations[i].second);
     };
 
-    setImages(current);
+    setImages(currentCombination);
 })
 
+function advanceImages(selection) {
+    console.log("You selected " + selection);
+    if(selectionsMap.hasOwnProperty(selection)) {
+        selectionsMap[selection] = selectionsMap[selection] + 1;
+    }
+    else {
+        selectionsMap[selection] = 1;
+    }
+
+    currentCombination += 1;
+
+    if(currentCombination < combinations.length) {
+        setImages(currentCombination);
+    }
+    else {
+        displaySummary();
+    }
+}
+
+function setImages(index) {
+    var combination = combinations[index];
+    $('#leftHidden').attr('value', combination.first);
+    $('#leftSelector').attr('src', 'images/' + 
+        combination.first + '.png');
+
+    $('#rightHidden').attr('value', combination.second);
+    $('#rightSelector').attr('src', 'images/' + 
+        combination.second + '.png');
+
+    $(".progressContainer").text(
+        (index + 1) + " of " + combinations.length);
+}
+
+function displaySummary() {
+    $("#heading").text("Results");
+    $(".selectorContainer").hide();
+    $(".progressContainer").hide();
+    $(".resultsContainer").show();
+
+    for(var key in selectionsMaap){
+      var value = selectionsMap[key];
+      console.log(key + ' = ' + value);
+      $("#resultsText").append("<p>" + key + " : " + value + "</p>");
+    }
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
